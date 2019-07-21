9c3f6f4ab60d0a3a107deea913122f3f38acf15c
Balu Naik <balu.naik@namshi.com>
2019-07-21 16:15:59 +0400

    Swift 4.2 migration

---
+++ Example/build/Pods.build/Debug-iphonesimulator/NMAnimatedTabBarItem.build/DerivedSources/NMAnimatedTabBarItem_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char NMAnimatedTabBarItemVersionString[];
+ extern const double NMAnimatedTabBarItemVersionNumber;
+
+ const unsigned char NMAnimatedTabBarItemVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:NMAnimatedTabBarItem  PROJECT:Pods-1" "\n";
+ const double NMAnimatedTabBarItemVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ Example/build/Pods.build/Debug-iphonesimulator/NMAnimatedTabBarItem.build/Objects-normal/x86_64/NMAnimatedTabBarItem_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/balunaik/Documents/NMAnimatedTabBarItem/Example/build/Pods.build/Debug-iphonesimulator/NMAnimatedTabBarItem.build/DerivedSources/NMAnimatedTabBarItem_vers.c
